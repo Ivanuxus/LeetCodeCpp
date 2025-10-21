98411f046568a28d1ba66e761798ed1ae44ce073
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-10-21 18:38:30 +0300

    Update 168.cpp

--- 168. Excel Sheet Column Title/168.cpp
+++ 168. Excel Sheet Column Title/168.cpp
@@ -1,44 +1,48 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string convertToTitle(int columnNumber) {
-		vector<int> result = {};
+	string convertToTitle(int columnNumber)
+	{
+
+		vector<int> result = {0, 0, 0, 0, 0, 0, 0, 0};
 		string a;
 		result.push_back(columnNumber);
-		char Alphabet[27] = {' ','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};				        
-		vector<int>::iterator it1 = result.end();
-		vector<int>::iterator it2;
-		while(it1 != result.begin()) {
-			bool isFirst = true;
-			if(*it1 / 27 > 0) {
-				*it1 -= 26;
-				if(isFirst){
-					it2 = result.begin();
-					result.insert(it2, 0);
-					isFirst = false;
-				}
-				*(it1 - 1) += 1;
-				
+		char Alphabet[27] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		int i = result.size() - 1;
+		cout << endl;
+		while (i != 0)
+		{
+			if (result[i] / 27 > 0)
+			{
+				result[i] -= 26;
+				result[i - 1] += 1;
 			}
-			if(*it1 < 27) {
-				it1--;
+			if (result[i] < 27)
+			{
+				i--;
 			}
-			if(it1 == result.begin())
-			{break;}
+			if (i <= 0)
+			{
+				break;
+			}
 		}
-		for(int j = 0; j < result.size(); j++) {
-			for(int k = 0; k < 27; k++) {
-		if(k == result[j] && result[j] != 0) {
-				a+= Alphabet[k];
-			}
+		for (int j = 0; j < result.size(); j++)
+		{
+			for (int k = 0; k < 27; k++)
+			{
+				if (k == result[j] && result[j] != 0)
+				{
+					a += Alphabet[k];
+				}
 			}
 		}
 		return a;
-    }
+	}
 };
 int main()
 {
@@ -46,5 +50,5 @@ int main()
 	int columnNumber;
 	scanf("%d", &columnNumber);
 	auto a = S.convertToTitle(columnNumber);
-	cout<<a;
+	cout << a;
 }
