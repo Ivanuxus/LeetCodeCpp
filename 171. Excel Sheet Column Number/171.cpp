f3250d57a496d499f0ec02462c789d989f07381b
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-10-22 10:02:47 +0300

    Update 171.cpp

--- 171. Excel Sheet Column Number/171.cpp
+++ 171. Excel Sheet Column Number/171.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Solution
 {
 public:
-	int convertToTitle(string columnTitle)
+	int titleToNumber(string columnTitle)
 	{
 		int result = 0;
 		string a;
@@ -33,13 +33,55 @@ public:
 		}
 		return result;
 	}
+	int convertToTitle(string Resulted)
+	{
+		vector<int> result = {0, 0, 0, 0, 0, 0, 0, 0};
+		int sum = 0;
+		char Alphabet[27] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		int i = result.size() - 1;
+		string a;
+		while (true)
+		{
+			string a = "";
+			auto end = result.size() - 1;
+			result[end] += 1;
+			if (result[end] == 27)
+			{
+				result[end - 1] += 1;
+				sum += result[end];
+				result[end] = 1;				
+			}
+			for (int j = 0; j < result.size(); j++)
+			{
+				for (int k = 0; k < 27; k++)
+				{
+					if (k == result[j] && result[j] != 0)
+					{
+						a += Alphabet[k];
+					}
+				}
+			}
+			cout << a;
+			if (a == Resulted)
+			{
+				for (int j = 0; j < result.size(); j++)
+				{
+					cout << result[j] << " ";
+					if(result[j] == 1){continue;}
+					sum += result[j];
+				}
+				return sum;
+			}
+		}
+	}
 };
 int main()
 {
 	Solution S;
 	string columnTitle = "";
 	cin >> columnTitle;
-	auto a = S.convertToTitle(columnTitle);
+	// auto a = S.titleToNumber(columnTitle);
+	auto a = S.convertToTitle("AB");
 	cout << endl;
 	cout << a;
 }
