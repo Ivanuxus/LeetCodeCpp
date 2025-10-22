dcf1dfc395b510270c0827dfd2fdc67b06386a81
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-10-22 13:25:51 +0300

    Update 171.cpp

--- 171. Excel Sheet Column Number/171.cpp
+++ 171. Excel Sheet Column Number/171.cpp
@@ -6,36 +6,9 @@ using namespace std;
 class Solution
 {
 public:
-	int titleToNumber(string columnTitle)
-	{
-		int result = 0;
-		string a;
-		char Alphabet[27] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-		string::iterator it;
-		int span = 0;
-		for (it = columnTitle.end(); it != columnTitle.begin() - 1; it--)
-		{
-			for (int i = 0; i < 27; i++)
-			{
-				if (*it == Alphabet[i])
-				{
-					for (int j = 1; j < 27; j++)
-					{
-						if (i == j)
-						{
-							break;
-						}
-						span += 25;
-					}
-					result += (i + span);
-				}
-			}
-		}
-		return result;
-	}
 	int convertToTitle(string Resulted)
 	{
-		vector<int> result = {0, 0, 0, 0, 0, 0, 0, 0};
+		vector<int> result = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		int sum = 0;
 		char Alphabet[27] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 		int i = result.size() - 1;
@@ -45,11 +18,14 @@ public:
 			string a = "";
 			auto end = result.size() - 1;
 			result[end] += 1;
-			if (result[end] == 27)
+			sum += 1;
+			for (int j = 1; j < result.size(); j++)
 			{
-				result[end - 1] += 1;
-				sum += result[end];
-				result[end] = 1;				
+				if (result[j] == 27)
+				{
+					result[j - 1] += 1;
+					result[j] = 1;
+				}
 			}
 			for (int j = 0; j < result.size(); j++)
 			{
@@ -61,16 +37,18 @@ public:
 					}
 				}
 			}
-			cout << a;
 			if (a == Resulted)
 			{
 				for (int j = 0; j < result.size(); j++)
 				{
 					cout << result[j] << " ";
-					if(result[j] == 1){continue;}
-					sum += result[j];
 				}
+				cout << a;
 				return sum;
+			}
+			else
+			{
+				string a = "";
 			}
 		}
 	}
@@ -79,9 +57,9 @@ int main()
 {
 	Solution S;
 	string columnTitle = "";
-	cin >> columnTitle;
+	// cin >> columnTitle;
 	// auto a = S.titleToNumber(columnTitle);
-	auto a = S.convertToTitle("AB");
+	auto a = S.convertToTitle("FXSHRXW");
 	cout << endl;
 	cout << a;
 }
