842b76dbf7f36807843c5e7b36b8428024d1a3c4
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-10-24 13:45:57 +0300

    Update 171.cpp

--- 171. Excel Sheet Column Number/171.cpp
+++ 171. Excel Sheet Column Number/171.cpp
@@ -6,60 +6,110 @@ using namespace std;
 class Solution
 {
 public:
-	int convertToTitle(string Resulted)
+	void fillToSizeOfResulted(vector<int> &result, string &Resulted)
 	{
-		vector<int> result = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		int sum = 0;
-		char Alphabet[27] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-		int i = result.size() - 1;
-		string a;
-		while (true)
+		if (Resulted.size() - result.size() > 2)
 		{
-			string a = "";
-			auto end = result.size() - 1;
-			result[end] += 1;
-			sum += 1;
-			for (int j = 1; j < result.size(); j++)
+			while (Resulted.size() != result.size())
 			{
-				if (result[j] == 27)
-				{
-					result[j - 1] += 1;
-					result[j] = 1;
-				}
-			}
-			for (int j = 0; j < result.size(); j++)
-			{
-				for (int k = 0; k < 27; k++)
-				{
-					if (k == result[j] && result[j] != 0)
-					{
-						a += Alphabet[k];
-					}
-				}
-			}
-			if (a == Resulted)
-			{
-				for (int j = 0; j < result.size(); j++)
-				{
-					cout << result[j] << " ";
-				}
-				cout << a;
-				return sum;
-			}
-			else
-			{
-				string a = "";
+				result.push_back(1);
 			}
 		}
+	}
+	string convertionFromVectorOfNumsToString(vector<int> &result, char *Alphabet)
+	{
+		string a = "";
+		for (int j = 0; j < result.size(); j++)
+		{
+			for (int k = 0; k < 27; k++)
+			{
+				if (k == result[j] && result[j] != 0)
+				{
+					a += Alphabet[k];
+				}
+			}
+		}
+		return a;
+	}
+	void makeTheSameNumber(vector<int> &result, string &Resulted, char *Alphabet)
+	{
+		while (true)
+		{
+			for (int i = 0; i < result.size(); i++)
+			{
+				while (Alphabet[result[i]] != Resulted[i])
+				{
+					result[i] += 1;
+				}
+			}
+			break;
+		}
+	}
+	int countNumOfRow(vector<int> &result, string &Resulted, char *Alphabet)
+	{
+		int i = 0;
+		int num = 0;
+		for (int j = 0; j < result.size(); j++)
+		{
+			if (result[j] != 0)
+			{
+				i = j;
+				break;
+			}
+		}
+		while (i < result.size() - 1)
+		{
+			if (result[i] > 0)
+			{
+				result[i + 1] += 26;
+				result[i] -= 1;
+			}
+			if (result[i] == 0)
+			{
+				i++;
+			}
+			if (result[i] < 0)
+			{
+				cout << "vse ploho";
+			}
+			if (i >= (result.size() - 1))
+			{
+				break;
+			}
+		}
+		return result[result.size() - 1];
+	}
+
+	int titleToNumber(string columnTitle)
+	{
+		vector<int> result = {};
+		int sum = 0;
+		char Alphabet[27] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		if (columnTitle.size() == 1)
+		{
+			for (int k = 0; k < 27; k++)
+			{
+				if (Alphabet[k] == columnTitle[0])
+				{
+					return k;
+				}
+			}
+		}
+		else
+		{
+			fillToSizeOfResulted(result, columnTitle);
+			int i = result.size() - 1;
+			makeTheSameNumber(result, columnTitle, Alphabet);
+			string a = convertionFromVectorOfNumsToString(result, Alphabet);
+			sum = countNumOfRow(result, columnTitle, Alphabet);
+			return sum;
+		}
+		return sum;
 	}
 };
 int main()
 {
 	Solution S;
 	string columnTitle = "";
-	// cin >> columnTitle;
-	// auto a = S.titleToNumber(columnTitle);
-	auto a = S.convertToTitle("FXSHRXW");
-	cout << endl;
-	cout << a;
+	cout << S.titleToNumber("FXSHRXW");
 }
