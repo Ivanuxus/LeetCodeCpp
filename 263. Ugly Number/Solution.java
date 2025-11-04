4dee6029e19251d84c6175904908a24791fa0526
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-11-04 13:03:12 +0300

    Create Solution.java

---
+++ 263. Ugly Number/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isUgly(int n) {
+        while (true) {
+            if (n <= 0) {
+                return false;
+            }
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            if (n % 3 == 0) {
+                n = n / 3;
+            }
+            if (n % 5 == 0) {
+                n = n / 5;
+            }
+            if (n == 1) {
+                return true;
+            }
+            if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0) {
+                return false;
+            }
+        }
+    }
+
+
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Solution Instance = new Solution();
+        int n = 8;
+        System.out.println(Instance.isUgly(n));
+    }
+}
